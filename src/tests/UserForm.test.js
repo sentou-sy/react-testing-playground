bf54318dc61d4009f33ba036ff027ddf411ba41e
Steve Beyer <beyer_steve@outlook.de>
2020-05-03 15:34:31 +0200

    test: <UserForm /> event handling

--- src/tests/UserForm.test.js
+++ src/tests/UserForm.test.js
@@ -1,3 +1,35 @@
-test("Test User Form", () => {
-    
+import React from "react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+import UserForm from "./../UserForm";
+
+afterEach(cleanup);
+
+test("<UserForm /> can handle Events corectly", () => {
+    const handleChange = jest.fn();
+    const handleSubmit = jest.fn();
+    const {container} = render(<UserForm 
+        onSubmit={handleSubmit}
+        onChange={handleChange}
+        user={{
+            firstName: "Max",
+            age: 19,
+        }}
+    />);
+
+    // simulate form submission
+    fireEvent.submit(container.querySelector("form"));
+    expect(handleSubmit).toBeCalled();
+
+    // simulate form change
+    fireEvent.change(container.querySelector("input[name=firstName]"), { 
+        target: {
+            value: "Moritz",
+        }
+    });
+    fireEvent.change(container.querySelector("input[name=age]"), {
+        target: {
+            value: "18",
+        }
+    });
+    expect(handleChange).toBeCalledTimes(2);
 });

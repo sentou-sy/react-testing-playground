2d6ff80cdb1f8605adaca8d2799d1f7cec9042bb
Steve Beyer <beyer_steve@outlook.de>
2020-05-07 20:54:24 +0200

    refactor: UserList.test.js

--- src/tests/UserList.test.js
+++ src/tests/UserList.test.js
@@ -2,19 +2,23 @@ import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import UserList from "./../UserList";
 
-afterEach(cleanup);
-
-test("<UserList /> can render correct amount of li elements", () => {
-    const handleRenderData = jest.fn();
-    const coll = [{name: "Max"}, {name: "Moritz"}];
-
-    const { container } = render(
+let utils, coll, handleRenderData;
+beforeEach(() => {
+    handleRenderData = jest.fn();
+    coll = [{name: "Max"}, {name: "Moritz"}];
+    // render App
+    utils = render(
         <UserList
             coll={coll} 
             renderData={handleRenderData}
         />
     );
+})
+afterEach(cleanup);
 
-    // assert 2 because it should render 2 li's because we have 2 objects in the collection
-    expect(container.querySelectorAll("li").length).toBe(2);
+describe("<UserList />", () => {
+    it("can render correct amount of li elements", () => {
+        // assert 2 because it should render 2 li's because we have 2 objects in the collection
+        expect(utils.getAllByRole("listitem").length).toBe(2);
+    })
 });

04c0301be6867b862a3942e56b10710bc5eb2356
Steve Beyer <beyer_steve@outlook.de>
2020-05-09 14:55:22 +0200

    test: counter reducer

--- src/withRedux/store.js
+++ src/withRedux/store.js
@@ -1,22 +1,9 @@
-import { createStore } from "redux";
-import { INCREMENT, DECREMENT } from './actionTypes';
+import { createStore, combineReducers } from "redux";
+import reducers from "./reducers";
 
-// reducer
-function counter(state = { counter: 0 }, action) {
-    switch (action.type) {
-        case INCREMENT:
-            return Object.assign(state, {
-                counter: state.counter + 1
-            });
-        case DECREMENT:
-            return Object.assign(state, {
-                counter: state.counter - 1
-            });
-        default:
-            return state;
-    }
-}
+const combinedReducers = combineReducers(reducers);
 
-let store = createStore(counter, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+// @ts-ignore
+let store = createStore(combinedReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
